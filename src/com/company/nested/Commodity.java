8c6778c35e22c185e4a245b593434479d4ab3ac6
proKenz0 <paslavskuy950@gmail.com>
2021-03-20 12:59:05 +0200

    Nested classes home work

---
+++ src/com/company/nested/Commodity.java
@@ -0,0 +1,114 @@
+package com.company.nested;
+
+import com.company.iteratorsComparable.comparators.ProductLengthComparator;
+import com.company.iteratorsComparable.comparators.ProductWeightComparator;
+import com.company.iteratorsComparable.comparators.ProductWidthComparator;
+import com.company.nested.products.Product;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Commodity {
+    private List<Product> products;
+
+    public Commodity() {
+        products = new ArrayList<>();
+    }
+
+    public Commodity(List<Product> products) {
+        this.products = new ArrayList<>(products);
+    }
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void removeProduct(Product product) {
+        products.remove(product);
+    }
+
+    public void removeProduct(int index) {
+        if(index < products.size() && index >= 0)
+            products.remove(index);
+    }
+
+    public void changeProduct(Product productOld, Product productNew) {
+        if (products.contains(productOld))
+            products.set(products.indexOf(productOld), productNew);
+    }
+
+    public void changeProduct(int index, Product productNew) {
+        if(index < products.size() && index >= 0)
+            products.set(index, productNew);
+    }
+
+    public void sortByName() {
+        Collections.sort(products);
+    }
+
+    public void sortByLength() {
+        Collections.sort(products, new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                if(o1.getLength() > o2.getLength())
+                    return 1;
+                else if(o1.getLength() < o2.getLength())
+                    return -1;
+                return 0;
+            }
+        });
+    }
+
+    public void sortByWidth() {
+        Collections.sort(products, new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                if(o1.getWidth() > o2.getWidth())
+                    return 1;
+                else if(o1.getWidth() < o2.getWidth())
+                    return -1;
+                return 0;
+            }
+        });
+    }
+
+    public void sortByWeight() {
+        Collections.sort(products, new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                if(o1.getWeight() > o2.getWeight())
+                    return 1;
+                else if(o1.getWeight() < o2.getWeight())
+                    return -1;
+                return 0;
+            }
+        });
+    }
+
+    public void printProducts(){
+        System.out.println("Products: ");
+
+        for(var product : products)
+            printProduct(products.indexOf(product));
+
+        System.out.println();
+    }
+
+    public void printProduct(int index) {
+        if(index < products.size() && index >= 0)
+        System.out.println("Name = %s   Length = %d     Width = %d     Weight = %d".formatted(
+                products.get(index).getName(), products.get(index).getLength(), products.get(index).getWidth(),
+                products.get(index).getWeight()
+        ));
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = new ArrayList<>(products);
+    }
+}
---
+++ src/com/company/nested/products/Product.java
@@ -0,0 +1,52 @@
+package com.company.nested.products;
+
+public abstract class Product implements Comparable<Product> {
+    private String name;
+    private int length;
+    private int width;
+    private int weight;
+
+    public Product(String name, int length, int width, int weight) {
+        this.name = name;
+        this.length = length;
+        this.width = width;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Product o) {
+        return this.name.compareTo(o.getName());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+}
